533cc4ee24b55516f10b4c8274e277793b879eba
nokken65 <unknown65182@gmail.com>
2024-01-21 13:02:34 +0300

    feat: add selectors

--- src/hooks/useWindowScroll/useWindowScroll.ts
+++ src/hooks/useWindowScroll/useWindowScroll.ts
@@ -2,39 +2,51 @@ import React from 'react'
 
 type Coords = { x: number; y: number }
 
-type ScrollToFn = ({
-  x,
-  y,
-  behavior
-}: {
+type ScrollToOptions = {
   x?: Coords['x']
   y?: Coords['y']
   behavior?: ScrollBehavior
-}) => void
+}
 
-const useWindowScroll = (): [
-  { readonly x: number; readonly y: number },
-  ScrollToFn
-] => {
-  const x = React.useSyncExternalStore(subscribe, getScrollXSnapshot)
-  const y = React.useSyncExternalStore(subscribe, getScrollYSnapshot)
+type XSelector = (x: Coords['x']) => Coords['x']
+type YSelector = (y: Coords['y']) => Coords['y']
 
-  const scrollTo: ScrollToFn = React.useCallback(({ x, y, behavior }) => {
-    window.scrollTo({ left: x, top: y, behavior })
+type UseWindowScrollConfig = {
+  xSelector?: XSelector
+  ySelector?: YSelector
+}
+
+const useWindowScroll = (config?: UseWindowScrollConfig) => {
+  const { xSelector = (x: number) => x, ySelector = (y: number) => y } =
+    config ?? {}
+
+  const x = React.useSyncExternalStore(subscribe, () =>
+    getScrollXSnapshot(xSelector)
+  )
+  const y = React.useSyncExternalStore(subscribe, () =>
+    getScrollYSnapshot(ySelector)
+  )
+
+  const scrollTo = React.useCallback((options?: ScrollToOptions) => {
+    window.scrollTo({
+      left: options?.x,
+      top: options?.y,
+      behavior: options?.behavior
+    })
   }, [])
 
   return [{ x, y }, scrollTo] as const
 }
 
-function getScrollXSnapshot(): number {
-  return window.scrollX
+function getScrollXSnapshot(selector: XSelector) {
+  return selector(window.scrollX)
 }
 
-function getScrollYSnapshot(): number {
-  return window.scrollY
+function getScrollYSnapshot(selector: YSelector) {
+  return selector(window.scrollY)
 }
 
-function subscribe(listener: VoidFunction): VoidFunction {
+function subscribe(listener: VoidFunction) {
   window.addEventListener('scroll', listener)
 
   return () => {
