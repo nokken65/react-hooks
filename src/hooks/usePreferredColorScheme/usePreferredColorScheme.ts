23c5e694547c2df32756d81fccba6ffcf56d8f2c
nokken65 <unknown65182@gmail.com>
2024-01-23 22:33:44 +0300

    docs: add jsdoc for usePreferredColorScheme

--- src/hooks/usePreferredColorScheme/usePreferredColorScheme.ts
+++ src/hooks/usePreferredColorScheme/usePreferredColorScheme.ts
@@ -2,6 +2,17 @@ import { useMediaQuery } from '../useMediaQuery'
 
 type PreferredColorScheme = 'light' | 'dark'
 
+/**
+ * @description Returns a preferred color scheme.
+ *
+ * @return {PreferredColorScheme}
+ * A preferred color scheme.
+ *
+ * @example
+ * ```js
+ * const preferredColorScheme = usePreferredColorScheme();
+ * ```
+ */
 const usePreferredColorScheme = () => {
   const isDarkColorScheme = useMediaQuery('(prefers-color-scheme: dark)')
   return isDarkColorScheme ? 'dark' : 'light'
